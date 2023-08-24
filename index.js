66d9cc710cfa0021de394e1b477cc5cdeb1a39c9
Suprajna Sharma <suprasubedi11@gmail.com>
2023-08-24 15:41:18 +0545

    loads html from public folder

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
 const app = express();
 
+import * as url from "url";
+// const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+import path from "path";
+
 import http from "http";
 const server = http.createServer(app);
 //inside server we loaded the object app
 
+const dirPath = path.join(__dirname, "./public/index.html");
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  //   res.send("<h1>Hello world</h1>");
+  res.sendFile(dirPath);
 });
 
 server.listen(3000, () => {
