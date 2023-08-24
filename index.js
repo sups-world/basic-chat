bcafab99ce27ac41431bf3ba4ccd61f0a9e23bc4
Suprajna Sharma <suprasubedi11@gmail.com>
2023-08-24 15:26:35 +0545

    created http server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express from "express";
+const app = express();
+
+import http from "http";
+const server = http.createServer(app);
+//inside server we loaded the object app
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+server.listen(3000, () => {
+  console.log(`server listening of port 3000`);
+});
